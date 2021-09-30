77ebd785d3ea71b9643b233e4204928813a6fb58
joaoazevedo872 <joaoazevedo872@gmail.com>
2021-09-29 23:14:43 -0300

    Teste aprovação manual no pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hi Everybody'));
+app.get('/', (req, res) => res.send('Agora a coisa vai! Teste Full'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
